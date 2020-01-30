838e8cb1293760e36b552ebcadd517ebb4f9a4cc
LucasFICR <jose.alves@a.ficr.edu.br>
2020-01-30 09:33:59 -0300

    inicio do resgate da apiGit

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
 const express = require("express");
 const app = express();
-const CurriculoController = require("./app/controller/CurriculoController")(dataAPI);
 
-
+//routes
 
 app.get("/api/curriculo", function(req, res) {
   res.render("OK...");
 });
 
-
-app.listen(8077, function() {
-  console.log("Servidor Rodando na url http://localhost:8077");
+app.get("/*", (req, res, next) => {
+  res.status(404).send("Erro 404, nada por aqui");
 });
 
-// usar axios para pegar os APIs github e facebook e aplicar no projeto;
-// colocr uma duas function devolvendo as infomações do 
-module.exports = (dataAPI) {
-    dataHub() => {
 
-      
-      if (--dataHub OK--) {
-
-        
-      } else {
-        console.log ('dataHub!' + err);
-      }
-
-        
-  
-    }
-    dataFace() => {
-        console.log ('dataFace OK!');
-
-    }
-  };
+app.listen(8077, ()=> {
+  console.log("tudo funfando no http://localhoste:8077");
+});
---
+++ app/services/ApiGithub.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const apiGit = {
+    baseUrl: ,
+    client_id: ,
+    client_secret:
+
+}
+
+class App extends Componet {
+    constructor(){
+        super();
+        this.state = {
+            githubData: []
+        }
+    }
+    
+} 
