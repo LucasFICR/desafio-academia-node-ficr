5d2b021feb3432a97c0bc0c87afefdc960314082
LucasFICR <jose.alves@a.ficr.edu.br>
2020-01-30 16:31:39 -0300

    criação da pasta src,

---
+++ src/app/controller/CurriculoController.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const CurriculoController = 
+
+function myFunction(p1, p2) {
+   return p1 * p2; 
+};
+
+    console.log(myFunction(4, 3));
+
+
+
+module.exports = new CurriculoController
