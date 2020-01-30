bc35cf3db5934d5cb6d18258fdf87fecb4ea0f5b
LucasFICR <jose.alves@a.ficr.edu.br>
2020-01-30 09:31:12 -0300

    axios instalado

--- router.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const curriculoController = require("./app/controller/CurriculoController");
-const routes = Router();
-
-routes.get("/api/curriculo", curriculoController.get);
-
-module.exports = routes;
