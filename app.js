678389cca07d4daa94771c1cce0db0a11a2f423a
LucasFICR <jose.alves@a.ficr.edu.br>
2020-01-30 03:03:29 -0200

    Pacote de arquivos

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const CurriculoController = require("./app/controller/CurriculoController")(dataAPI);
 
- app.get("/", (req, res, next) => {
-    res.send("Tudo bem por aqui...");
 
+
+app.get("/api/curriculo", function(req, res) {
+  res.render("OK...");
 });
 
-  app.listen(9669, () => {
-    console.log("Servidor Rodando na url http://localhost:9669");
-  });
+
+app.listen(8077, function() {
+  console.log("Servidor Rodando na url http://localhost:8077");
+});
+
+// usar axios para pegar os APIs github e facebook e aplicar no projeto;
+// colocr uma duas function devolvendo as infomações do 
+module.exports = (dataAPI) {
+    dataHub() => {
+
+      
+      if (--dataHub OK--) {
+
+        
+      } else {
+        console.log ('dataHub!' + err);
+      }
+
+        
+  
+    }
+    dataFace() => {
+        console.log ('dataFace OK!');
+
+    }
+  };
---
+++ router.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const curriculoController = require("./app/controller/CurriculoController");
+const routes = Router();
+
+routes.get("/api/curriculo", curriculoController.get);
+
+module.exports = routes;
