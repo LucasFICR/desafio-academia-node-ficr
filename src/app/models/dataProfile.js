2300e03564a1c653eec2067aab694bb5367a7a01
LucasFICR <jose.alves@a.ficr.edu.br>
2020-02-02 00:57:04 -0300

    informações curriculares adicionadas a pasta model

---
+++ src/app/models/dataProfile.js
@@ -0,0 +1,21 @@
+const dataProfile = {
+    Formação: [
+      {
+        Instituição: "Faculdade Imaculada Conceição do Recife",
+        Curso: "Sistemas para Internet",
+        Inicio: "2019.2",
+        Termino: "2021.1",
+      },
+    ],
+    Profissional: [
+      {
+        Empresa: "Autônomo",
+        Função: "Suporte Técnico",
+        Atividade: "Prevenção e manutenção em Computadores.",
+        Inicio: "01/01/2014",
+        Termino: "atual"
+      }
+    ],  
+  };
+  
+  module.exports = dataProfile;
